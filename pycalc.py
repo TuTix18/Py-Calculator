ca9cbf24e30362fc1c8c96cc22d62448013efe01
TuTix18 <58310601+TuTix18@users.noreply.github.com>
2019-11-28 17:27:29 -0300

    Create pycalc.py

---
+++ pycalc.py
@@ -0,0 +1,24 @@
+# pycalc v1
+
+print("******************")
+print("PyCalculator v1 ")
+print("By: TuTix18")
+print("******************")
+
+print("Put the numbers below")
+x = float(input())
+signal = (input())
+y = float(input())
+
+if (signal == '+'):
+    print(x + y)
+elif (signal == '-'):
+    print(x - y)
+elif (signal == '*'):
+    print (x * y)
+elif (signal == '/'):
+    print (x / y)
+elif (signal == '**'):
+    print(x ** y)
+elif (signal == '%'):
+    print (x % y)
