50493a388e0ac472631ab92fa9568133a9abf0b2
TuTix18 <58310601+TuTix18@users.noreply.github.com>
2019-11-29 19:25:48 -0300

    Update 0.1.5
    Added color to texts.

--- pycalc.py
+++ pycalc.py
@@ -1,14 +1,19 @@
 from os import system
 
+# Color settings
+class colors:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+
 print("******************")
 print("PyCalculator v1 ")
 print("By: TuTix18")
 print("******************")
+print("Enter the numbers below")
 
-print("Put the numbers below")
-x = float(input("First Number "))
-signal = (input(" Signal ('+', '-', '*', '**', '/', '%') "))
-y = float(input("Second Number "))
+x = float(input(colors.GREEN + "First Number "))
+signal = (input(colors.GREEN + "Signal ('+', '-', '*', '**', '/', '%') "))
+y = float(input(colors.GREEN + "Second Number "))
 
 if signal == '+':
     print(x + y)
@@ -21,8 +26,8 @@ elif signal == '/':
 elif signal == '**':
     print(x ** y)
 elif signal == '%':
-    print (x % y)
+    print(x % y)
 else:
-  print("Please enter a valid signal")
+  print(colors.RED + "Please enter a valid signal")
 
 system("pause")
