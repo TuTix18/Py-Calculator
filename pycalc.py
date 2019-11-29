c8808599a1ffccbd37fc074f7bbe290fbc35e14f
TuTix18 <58310601+TuTix18@users.noreply.github.com>
2019-11-29 19:45:55 -0300

    Update 0.1.6

--- pycalc.py
+++ pycalc.py
@@ -1,33 +1,33 @@
-from os import system
-
 # Color settings
 class colors:
     GREEN = '\033[92m'
     RED = '\033[91m'
 
-print("******************")
-print("PyCalculator v1 ")
-print("By: TuTix18")
-print("******************")
-print("Enter the numbers below")
+# main program
+while True:
+    print() # prints a new line
+    print("******************")
+    print("PyCalculator v1 ")
+    print("By: TuTix18")
+    print("******************")
+    print()
+    print("Enter the numbers below")
 
-x = float(input(colors.GREEN + "First Number "))
-signal = (input(colors.GREEN + "Signal ('+', '-', '*', '**', '/', '%') "))
-y = float(input(colors.GREEN + "Second Number "))
+    x = float(input(colors.GREEN + "First Number "))
+    signal = (input(colors.GREEN + "Signal ('+', '-', '*', '**', '/', '%') "))
+    y = float(input(colors.GREEN + "Second Number "))
 
-if signal == '+':
-    print(x + y)
-elif signal == '-':
-    print(x - y)
-elif signal == '*':
-    print (x * y)
-elif signal == '/':
-    print (x / y)
-elif signal == '**':
-    print(x ** y)
-elif signal == '%':
-    print(x % y)
-else:
-  print(colors.RED + "Please enter a valid signal")
-
-system("pause")
+    if signal == '+':
+        print(x + y)
+    elif signal == '-':
+        print(x - y)
+    elif signal == '*':
+        print (x * y)
+    elif signal == '/':
+        print (x / y)
+    elif signal == '**':
+        print(x ** y)
+    elif signal == '%':
+        print(x % y)
+    else:
+        print(colors.RED + "Please enter a valid signal")
